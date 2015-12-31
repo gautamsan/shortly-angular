5b3713dc83d4f55357a3607be20aa5927a36b74c
Santosh Gautam <santoshgautam7@gmail.com>
2015-12-31 00:04:07 -0800

    ShortenLinks working.

--- client/app/app.js
+++ client/app/app.js
@@ -24,7 +24,7 @@ angular.module('shortly', [
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
     })
-    .otherwise('/')
+    .otherwise('/links')
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
@@ -56,8 +56,11 @@ angular.module('shortly', [
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    console.log(evt);
-    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+    console.log(next.$$route);
+    console.log(!Auth.isAuth());
+
+    // if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+    if (next.$$route && next.$$route.controller !== "AuthController" && !Auth.isAuth()) {
       $location.path('/signin');
     }
   });
